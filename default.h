65c6a617dd20bea81c69699cda00e30252269585
fujidori <jijiji65535ffff@gmail.com>
2018-06-25 19:25:45 +0900

    Rename the macro name

--- default.h
+++ default.h
@@ -1,5 +1,5 @@
-#ifndef _SHARED_H
-#define _SHARED_H
+#ifndef _DEFAULT_H
+#define _DEFAULT_H
 
 #define	SERVICE_ADDR	"127.0.0.1"	/* default server address */
 #define	SERVICE_PORT	"12345"	/* default port number */
